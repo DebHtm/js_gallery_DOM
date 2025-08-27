448ecc3f73bfdbd35759c0056cdc9b85f79203f1
Denys <yolm9yom@gmail.com>
2025-08-27 17:11:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbs = document.querySelectorAll('.list-item a');
+const bigImg = document.querySelector('.gallery img');
+
+thumbs.forEach((a) => {
+  a.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let target = e.target;
+
+    if (target.tagName === 'IMG') {
+      target = target.parentElement;
+    }
+
+    if (target.tagName === 'A') {
+      bigImg.src = target.href;
+    }
+  });
+});
